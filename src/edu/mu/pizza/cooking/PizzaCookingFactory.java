394168655098aa86e26e28fc9c33852a0175268c
Dorion-M <damcbv@umsystem.edu>
2024-03-08 11:06:28 -0600

    Created PizzaCookingFactory

--- src/edu/mu/pizza/cooking/PizzaCookingFactory.java
+++ src/edu/mu/pizza/cooking/PizzaCookingFactory.java
@@ -1,5 +1,27 @@
 package edu.mu.pizza.cooking;
 
+import edu.mu.pizza.AbstractPizza;
+import edu.mu.pizza.PizzaType;
+import edu.mu.pizza.specialty.*;
+
+
 public class PizzaCookingFactory {
+	
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+        AbstractPizza pizza = null;
+        switch (pizzaType) {
+            case MARGHERITA:
+                pizza = new MargheritaPizza();
+                break;
+            case VEGETARIAN:
+                pizza = new VegetarianPizza();
+                break;
+            case HAWAIIAN:
+            	pizza = new HawaiianPizza();
+            case SUPREME:
+            	pizza = new SupremePizza();
+        }
+        return pizza;
+    }
 
 }
